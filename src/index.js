deabaadbf6bac08a5cbf6bc13ac4c70a0a2d8da8
Steve M <uziandari@gmail.com>
2017-01-19 09:48:29 -0500

    initial commit, project setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './scenes/Home/index';
+import './styles';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('report-viewer')
+);
